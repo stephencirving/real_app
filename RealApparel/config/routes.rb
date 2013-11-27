67b235777dcb6f64e7bbcde74b1a0a219d8ceb9e
Stephen Irving <stephen.c.irving@gmail.com>
2013-11-27 20:20:13 +1100

    Campaign_model_bootstrap_fixed_set_up_and_devise_installed_but_not_migrated

--- RealApparel/config/routes.rb
+++ RealApparel/config/routes.rb
@@ -1,4 +1,5 @@
 Campaigns::Application.routes.draw do
+  devise_for :users
   root 'campaigns#index'
   resources :campaigns
   # The priority is based upon order of creation: first created -> highest priority.
